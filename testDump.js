559993ced464157737ad729fe1242685a34fc99b
Bill Marcy <william_marcy@brown.edu>
2014-11-03 23:13:51 -0500

    Recmmitted due to git issues

---
+++ testDump.js
@@ -0,0 +1,21 @@
+function ex() {
+	document.getElementById("test").innerHTML += "!";
+}
+function qu() {
+	document.getElementById("test").innerHTML += "?";
+}
+function hash() {
+	document.getElementById("test").innerHTML += "#";
+}
+function ast() {
+	document.getElementById("test").innerHTML += "*";
+}
+function red() {
+	document.getElementById("test").style.color = "red";
+}
+function green() {
+	document.getElementById("test").style.color = "green";
+}
+function blue() {
+	document.getElementById("test").style.color = "blue";
+}
