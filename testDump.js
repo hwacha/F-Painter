8fe293c02e7235beae5ef55ce28048257b4caf40
Bill Marcy <william_marcy@brown.edu>
2014-11-08 14:15:09 -0500

    Removed "move" directory

--- testDump.js
+++ testDump.js
@@ -1,3 +1,9 @@
+var symbols = [];
+var i;
+for(i = 0; i < 1000; i++) {
+	symbols[i] = String.fromCharCode(56 + (i % 15));
+}
+
 function ex() {
 	document.getElementById("test").innerHTML += "!";
 }
@@ -18,4 +24,10 @@ function green() {
 }
 function blue() {
 	document.getElementById("test").style.color = "blue";
+}
+function dollar() {
+	var i;
+	for(i = 0; i < symbols.length; i++) {
+		document.getElementById("test").innerHTML += symbols[i];
+	}
 }
